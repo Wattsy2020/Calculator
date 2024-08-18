fedd0a557de5220b53c5c08c32ee3495f710a975
Liam Watts <liam.watts271@gmail.com>
2024-08-18 11:47:11 +1000

    app: support editing the input using arrow keys

--- app/Console.hs
+++ app/Console.hs
@@ -2,13 +2,19 @@ module Console (interactLines) where
 
 import System.Console.ANSI
 import System.IO
+import Data.Sequence qualified as Seq
+import Data.Foldable ( Foldable(toList) )
 
 data Key = 
   UpArrow
   | DownArrow
+  | LeftArrow
+  | RightArrow
   | Delete
   | Char Char
   deriving Show
+
+type SeqString = Seq.Seq Char
 
 (!?) :: [a] -> Int -> Maybe a
 [] !? _ = Nothing
@@ -29,6 +35,8 @@ getKey = do
       return $ case (char2, char3) of
         ('[', 'A') -> UpArrow
         ('[', 'B') -> DownArrow
+        ('[', 'C') -> RightArrow
+        ('[', 'D') -> LeftArrow
         _ -> Char char2 -- should handle the other escape sequences
     _ -> return $ Char char
 
@@ -40,36 +48,45 @@ resetLine = clearLine >> setCursorColumn 0
 resetLineTo :: String -> IO ()
 resetLineTo string = resetLine >> putStr string
 
+-- convert SeqString to a string
+convert :: SeqString -> String
+convert = toList
+
 -- show an input and continue the input loop
-showInput :: (String -> String) -> String -> [String] -> Int -> IO ()
-showInput f input previousInputs inputIdx = do 
-  resetLineTo $ reverse input
-  interactLines' f input previousInputs inputIdx
+showInput :: (String -> String) -> SeqString -> [SeqString] -> Int -> Int -> IO ()
+showInput f input previousInputs inputIdx colIdx = do 
+  resetLineTo $ convert input
+  setCursorColumn colIdx
+  interactLines' f input previousInputs inputIdx colIdx
 
 -- show a previous input given the idx of that input
-showInputIdx :: (String -> String) -> String -> [String] -> Int -> Int -> IO ()
+showInputIdx :: (String -> String) -> SeqString -> [SeqString] -> Int -> Int -> IO ()
 showInputIdx f input previousInputs inputIdx prevIdx = case previousInputs !? inputIdx of
-  Nothing -> showInput f input previousInputs prevIdx
-  Just selectedInput -> showInput f selectedInput previousInputs inputIdx
+  Nothing -> showInput f input previousInputs prevIdx (length input) -- reset the cursor to point to the end of the line
+  Just selectedInput -> showInput f selectedInput previousInputs inputIdx (length selectedInput)
 
-interactLines' :: (String -> String) -> String -> [String] -> Int -> IO ()
-interactLines' f input previousInputs inputIdx = do
+interactLines' :: (String -> String) -> SeqString -> [SeqString] -> Int -> Int -> IO ()
+interactLines' f input previousInputs inputIdx colIdx = do
   key <- getKey
   case key of
-    UpArrow -> showInputIdx f input previousInputs (inputIdx + 1) inputIdx
+    UpArrow -> showInputIdx f input previousInputs (inputIdx + 1) inputIdx 
     DownArrow -> showInputIdx f input previousInputs (inputIdx - 1) inputIdx
-    Delete -> let newInput = drop 1 input in 
-      showInput f newInput previousInputs inputIdx
+    LeftArrow -> showInput f input previousInputs inputIdx (max (colIdx - 1) 0)
+    RightArrow -> showInput f input previousInputs inputIdx (min (colIdx + 1) (length input))
+    Delete -> if colIdx == 0
+      then interactLines' f input previousInputs inputIdx colIdx
+      else showInput f (Seq.deleteAt (colIdx - 1) input) previousInputs inputIdx (colIdx - 1)
     Char '\n' -> do
-      putStrLn $ f $ reverse input
-      interactLines' f "" (input : previousInputs) (-1)
-    Char char -> interactLines' f (char : input) previousInputs (-1)
+      putStrLn $ f $ convert input
+      interactLines' f Seq.empty (input : previousInputs) (-1) 0
+    Char char -> showInput f (Seq.insertAt colIdx char input) previousInputs (-1) (colIdx + 1)
 
 -- Start a simple CLI program
 -- Each line of input is passed to f, the output of f is then printed
--- It also supports using the Up and Down arrow keys to select previous inputs
+-- It supports using the Up and Down arrow keys to select previous inputs
+-- And using the Left and Right arrow keys, and the Delete key, to edit the current input 
 interactLines :: (String -> String) -> IO ()
 interactLines f = do 
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
-  interactLines' f "" [] (-1)
+  interactLines' f Seq.Empty [] (-1) 0
